4587ad2ed3e5dd43eb00c0fd90b029bd2909f0fd
FrontX-Ganesh <ganeshmaharnur11@gmail.com>
2024-04-28 12:51:03 +0530

    color and svg change

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './services.css';
 import { FaCircleCheck } from "react-icons/fa6";
-import { FaRegTimesCircle } from "react-icons/fa";
+import { MdOutlineRemoveCircle } from "react-icons/md";
 
 const Services = () => {
   return (
@@ -17,29 +17,29 @@ const Services = () => {
             <div className='row'>
                 <div className='col-md-4'>Non surgical</div>
                 <div className='col-md-2'><FaCircleCheck /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
             </div>
             <div className='row'>
                 <div className='col-md-4'>Immediate Result</div>
                 <div className='col-md-2'><FaCircleCheck /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
             </div>
             <div className='row'>
                 <div className='col-md-4'>Low Maintenance</div>
                 <div className='col-md-2'><FaCircleCheck /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
             </div>
             <div className='row'>
                 <div className='col-md-4'>Affordable</div>
                 <div className='col-md-2'><FaCircleCheck /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
             </div>
             <div className='row'>
@@ -47,7 +47,7 @@ const Services = () => {
                 <div className='col-md-2'><FaCircleCheck /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
                 <div className='col-md-2'><FaCircleCheck /></div>
-                <div className='col-md-2'><FaRegTimesCircle /></div>
+                <div className='col-md-2'><MdOutlineRemoveCircle style={{ color: 'red', fontSize: '25px' }} /></div>
             </div>
         </div>
     </>
